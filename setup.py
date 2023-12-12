c360d72ea35d76f33d9daf0a4ba0dcf2e15cc4c0
Shuxin-Lin <Shuxin.Lin@ibm.com>
2023-12-12 11:57:04 -0500

    fix(setup.py): update to include requirements.txt in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 
+
+with open('requirements.txt') as f:
+    required_pkgs = f.read().splitlines()
+
 setup(
     name='autorecipe',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-        # List your dependencies here
-    ],
+    install_requires=required_pkgs,
 )
