c08e74123260cc67414269821041bc94e0986e02
Dhaval <pateldha@us.ibm.com>
2024-04-02 21:18:13 -0400

    updated code

--- fmea_recommender/analysis_dcp.py
+++ fmea_recommender/analysis_dcp.py
@@ -4,12 +4,6 @@ import mlflow
 import uuid
 from autorecipe.genai.GenAIChat import GenAIChatClient
 
-from genai.client import Client
-from genai.credentials import Credentials
-from genai.schema import (
-    TextGenerationParameters,
-    TextGenerationReturnOptions,
-)
 from genai.schema import (
     DecodingMethod,
     ModerationHAP,
@@ -42,13 +36,13 @@ params = {
 }
 model_id = 1
 
-df_train = pd.read_csv('../fmea_result/autoQ_train_data_input_for_experiments.csv')
-df_val = pd.read_csv('../fmea_result/autoQ_val_data_input_for_experiments.csv')
-df_test = pd.read_csv('../fmea_result/autoQ_test_data_input_for_experiments.csv')
+df_train = pd.read_csv("../fmea_result/autoQ_train_data_input_for_experiments.csv")
+df_val = pd.read_csv("../fmea_result/autoQ_val_data_input_for_experiments.csv")
+df_test = pd.read_csv("../fmea_result/autoQ_test_data_input_for_experiments.csv")
 
-type_col = 'TypeData.GenCompType'
-long_desc_col = 'long_description'
-failure_loc_col = 'failure_locations'
+type_col = "TypeData.GenCompType"
+long_desc_col = "long_description"
+failure_loc_col = "failure_locations"
 
 template = """
 I have an industrial asset, its long description and the locations where the asset can fails. 
@@ -63,25 +57,32 @@ Failure Locations: {failureloc}
 
 """
 
+
 def get_prompts(df):
-    '''
-    '''
+    """ """
     prompts = []
     for _, row in df.iterrows():
         asset = row[type_col]
         long_desc = row[long_desc_col]
-        failure_loc = str(eval(row[failure_loc_col])).replace('{', '').replace('}', '').replace("'", "")
-        prompt = template.format(asset=asset,longdesc=long_desc,failureloc=failure_loc)
+        failure_loc = (
+            str(eval(row[failure_loc_col]))
+            .replace("{", "")
+            .replace("}", "")
+            .replace("'", "")
+        )
+        prompt = template.format(
+            asset=asset, longdesc=long_desc, failureloc=failure_loc
+        )
         prompts.append(prompt)
     return prompts
+
 
 train_prompts = get_prompts(df_train)
 val_prompts = get_prompts(df_val)
 test_prompts = get_prompts(df_test)
 
-print (test_prompts)
-
 ray.init(num_cpus=8)
+
 
 @ray.remote
 def get_summaries(prompt):
@@ -89,54 +90,58 @@ def get_summaries(prompt):
     experiment_name = "MyExperiment_" + str(uuid.uuid4())
     experiment_id = mlflow.create_experiment(experiment_name)
 
-    tmpClient = GenAIChatClient(name='Testing',
-                                    description='I am testing classifier',
-                                    skill='Generic',
-                                    model=LLMsets[model_id],
-                                    params=params,
-                                    credentials=creds,
-                                    system_message=prompt,
-                                    stateful=True,
-                                    stream=True)
+    tmpClient = GenAIChatClient(
+        name="Testing",
+        description="I am testing classifier",
+        skill="Generic",
+        model=LLMsets[model_id],
+        params=params,
+        credentials=creds,
+        system_message=prompt,
+        stateful=True,
+        stream=True,
+    )
 
     answer = tmpClient.create(
-            context=None,
-            messages=[{"content": "Here is a readable Summary of Failure Locations:\n", "role": "user"}],
-            experiment_id=experiment_id,
-        )
+        context=None,
+        messages=[
+            {
+                "content": "Here is a readable Summary of Failure Locations:\n",
+                "role": "user",
+            }
+        ],
+        experiment_id=experiment_id,
+    )
 
-    #"Did i miss any failure locations which is not captured in the Failure Locations?"
-    #"can you improve your answer by grouping the failure locations into some category?"
-    #"can you generate a redable summary of the above answer?"
-    #"can you provide any thinking why these are the failure location?"
-    #"generate list of failure locations"
+    # "Did i miss any failure locations which is not captured in the Failure Locations?"
+    # "can you improve your answer by grouping the failure locations into some category?"
+    # "can you generate a redable summary of the above answer?"
+    # "can you provide any thinking why these are the failure location?"
+    # "generate list of failure locations"
 
     return answer.strip()
 
 
-remote_call = []
-for pt in train_prompts[:1]:
-    remote_call.append(get_summaries.remote(pt))
-train_summaries = ray.get(remote_call)
-print('train generated')
-print (train_summaries[0])
-exit(0)
+for model_id in [1, 2, 3]:
+    model_initial = LLMsets[model_id].split("/")[1].split("-")[0]
 
-remote_call = []
-for pt in val_prompts:
-    remote_call.append(get_summaries.remote(pt))
-val_summaries = ray.get(remote_call)
-print('val generated')
+    remote_call = []
+    for pt in train_prompts:
+        remote_call.append(get_summaries.remote(pt))
+    train_summaries = ray.get(remote_call)
+    df_train["answer"] = train_summaries
+    df_train.to_csv("train_" + model_initial + ".csv")
 
-remote_call = []
-for pt in test_prompts:
-    remote_call.append(get_summaries.remote(pt))
-test_summaries = ray.get(remote_call)
-print('test generated')
+    remote_call = []
+    for pt in val_prompts:
+        remote_call.append(get_summaries.remote(pt))
+    val_summaries = ray.get(remote_call)
+    df_val["answer"] = train_summaries
+    df_val.to_csv("val_" + model_initial + ".csv")
 
-
-# In[ ]:
-
-
-
-
+    remote_call = []
+    for pt in test_prompts:
+        remote_call.append(get_summaries.remote(pt))
+    test_summaries = ray.get(remote_call)
+    df_test["answer"] = train_summaries
+    df_test.to_csv("test_" + model_initial + ".csv")
