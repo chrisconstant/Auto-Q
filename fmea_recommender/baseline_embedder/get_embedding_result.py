c53994a1a7a772db587b39ff59de2d6d74281022
Dhaval <pateldha@us.ibm.com>
2024-05-04 21:50:13 -0400

    updated

--- fmea_recommender/baseline_embedder/get_embedding_result.py
+++ fmea_recommender/baseline_embedder/get_embedding_result.py
@@ -9,18 +9,18 @@ l2v = LLM2Vec.from_pretrained(
     torch_dtype=torch.bfloat16,
 )
 
-df = pd.read_csv('./data/all_ids_deduplicated_train_short_desc_to_failure_locations.csv')
-documents = list(df['TypeData.GenCompType'])
+prefixmatch = 'Mistral_unsup_simcse'
+
+df = pd.read_csv(
+    "./data/all_ids_deduplicated_train_short_desc_to_failure_locations.csv"
+)
+documents = list(df["TypeData.GenCompType"])
 d_reps = l2v.encode(documents)
 
-df = pd.read_csv('./data/all_ids_deduplicated_test_short_desc_to_failure_locations.csv')
-querys = list(df['TypeData.GenCompType'])
-instruction = (
-    "Given an industrial equipment, retrieve similar equipment:"
-)
-queries = [
-    instruction + " " + item for item in querys
-]
+df = pd.read_csv("./data/all_ids_deduplicated_test_short_desc_to_failure_locations.csv")
+querys = list(df["TypeData.GenCompType"])
+instruction = "Given an industrial equipment, retrieve similar equipment:"
+queries = [instruction + " " + item for item in querys]
 q_reps = l2v.encode(queries)
 
 # passing the results
@@ -29,18 +29,25 @@ d_reps_norm = torch.nn.functional.normalize(d_reps, p=2, dim=1)
 cos_sim = torch.mm(q_reps_norm, d_reps_norm.transpose(0, 1))
 
 top_values, top_indices = torch.topk(cos_sim, k=3, dim=1)
+top_values_list = top_values.tolist()
+top_indices_list = top_indices.tolist()
 
-print (top_values)
-print (top_indices)
-
-df = pd.read_csv('./data/all_ids_deduplicated_val_short_desc_to_failure_locations.csv')
-querys = list(df['TypeData.GenCompType'])
-instruction = (
-    "Given an industrial equipment, retrieve similar equipment:"
+# Convert lists to pandas DataFrames
+df_values = pd.DataFrame(
+    top_values_list, columns=["Top Value 1", "Top Value 2", "Top Value 3"]
 )
-queries = [
-    instruction + " " + item for item in querys
-]
+df_indices = pd.DataFrame(
+    top_indices_list, columns=["Top Index 1", "Top Index 2", "Top Index 3"]
+)
+
+# Concatenate DataFrames horizontally (along columns)
+df_merged = pd.concat([df_values, df_indices], axis=1)
+df_merged.to_csv(prefixmatch + "_test.csv", index=False)
+
+df = pd.read_csv("./data/all_ids_deduplicated_val_short_desc_to_failure_locations.csv")
+querys = list(df["TypeData.GenCompType"])
+instruction = "Given an industrial equipment, retrieve similar equipment:"
+queries = [instruction + " " + item for item in querys]
 q_reps = l2v.encode(queries)
 
 # passing the results
@@ -49,6 +56,17 @@ d_reps_norm = torch.nn.functional.normalize(d_reps, p=2, dim=1)
 cos_sim = torch.mm(q_reps_norm, d_reps_norm.transpose(0, 1))
 
 top_values, top_indices = torch.topk(cos_sim, k=3, dim=1)
+top_values_list = top_values.tolist()
+top_indices_list = top_indices.tolist()
 
-print (top_values)
-print (top_indices)
+# Convert lists to pandas DataFrames
+df_values = pd.DataFrame(
+    top_values_list, columns=["Top Value 1", "Top Value 2", "Top Value 3"]
+)
+df_indices = pd.DataFrame(
+    top_indices_list, columns=["Top Index 1", "Top Index 2", "Top Index 3"]
+)
+
+# Concatenate DataFrames horizontally (along columns)
+df_merged = pd.concat([df_values, df_indices], axis=1)
+df_merged.to_csv(prefixmatch + "_val.csv", index=False)
