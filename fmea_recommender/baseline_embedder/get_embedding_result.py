c3fe19c517f8e3875bd6817be9e6e0e112e2e6f0
Dhaval <pateldha@us.ibm.com>
2024-05-06 21:23:45 -0400

    updated code

--- fmea_recommender/baseline_embedder/get_embedding_result.py
+++ fmea_recommender/baseline_embedder/get_embedding_result.py
@@ -4,50 +4,51 @@ from llm2vec import LLM2Vec
 from sentence_transformers import SentenceTransformer
 
 sets = [
-    {'train': "./data/all_ids_deduplicated_train_short_desc_to_failure_locations.csv",
-     'dest': "./data/all_ids_deduplicated_val_short_desc_to_failure_locations.csv",
-     'mode': 'val',
-     },
-    {'train': "./data/all_ids_deduplicated_train_short_desc_to_failure_locations.csv",
-     'dest': "./data/all_ids_deduplicated_test_short_desc_to_failure_locations.csv",
-     'mode': 'test',
-     },
+    {
+        "train": "./data/all_ids_deduplicated_train_short_desc_to_failure_locations.csv",
+        "dest": "./data/all_ids_deduplicated_val_short_desc_to_failure_locations.csv",
+        "mode": "val",
+    },
+    {
+        "train": "./data/all_ids_deduplicated_train_short_desc_to_failure_locations.csv",
+        "dest": "./data/all_ids_deduplicated_test_short_desc_to_failure_locations.csv",
+        "mode": "test",
+    },
 ]
 
 models = [
-    {'mdl': "McGill-NLP/LLM2Vec-Mistral-7B-Instruct-v2-mntp",
-     'peft_model_name_or_path': "McGill-NLP/LLM2Vec-Mistral-7B-Instruct-v2-mntp-unsup-simcse",
-     'mode': 'Mistral_7B_Instruct_v2_mntp_unsup_simcse',
-     'type': 'LLM2vec'
-     },
-    {'mdl': "McGill-NLP/LLM2Vec-Mistral-7B-Instruct-v2-mntp",
-     'peft_model_name_or_path': "McGill-NLP/LLM2Vec-Mistral-7B-Instruct-v2-mntp-supervised",
-     'mode': 'Mistral_7B_Instruct_v2_mntp_supervised',
-     'type': 'LLM2vec'
-     },
-    {'mdl': "all-mpnet-base-v2",
-     'mode': 'all_mpnet_base_v2',
-     'type': 'ST'
-     },
+    {
+        "mdl": "McGill-NLP/LLM2Vec-Mistral-7B-Instruct-v2-mntp",
+        "peft_model_name_or_path": "McGill-NLP/LLM2Vec-Mistral-7B-Instruct-v2-mntp-unsup-simcse",
+        "mode": "Mistral_7B_Instruct_v2_mntp_unsup_simcse",
+        "type": "LLM2vec",
+    },
+    {
+        "mdl": "McGill-NLP/LLM2Vec-Mistral-7B-Instruct-v2-mntp",
+        "peft_model_name_or_path": "McGill-NLP/LLM2Vec-Mistral-7B-Instruct-v2-mntp-supervised",
+        "mode": "Mistral_7B_Instruct_v2_mntp_supervised",
+        "type": "LLM2vec",
+    },
+    {"mdl": "all-mpnet-base-v2", "mode": "all_mpnet_base_v2", "type": "ST"},
 ]
 
 for db in sets:
     for mdl in models:
-        if mdl['type'] == 'LLM2vec':
+        if mdl["type"] == "LLM2vec":
             l2v = LLM2Vec.from_pretrained(
-                mdl['mdl'],
-                peft_model_name_or_path=mdl['peft_model_name_or_path'],
+                mdl["mdl"],
+                peft_model_name_or_path=mdl["peft_model_name_or_path"],
                 device_map="cuda" if torch.cuda.is_available() else "cpu",
                 torch_dtype=torch.bfloat16,
             )
 
-            prefixmatch = mdl['mode'] + '_' + db['mode']
+            prefixmatch = mdl["mode"] + "_" + db["mode"]
 
-            df = pd.read_csv(db['train'])
+            df = pd.read_csv(db["train"])
             documents = list(df["TypeData.GenCompType"])
             d_reps = l2v.encode(documents)
 
-            df = pd.read_csv(db['dest'])
+            df = pd.read_csv(db["dest"])
             querys = list(df["TypeData.GenCompType"])
             instruction = "Given an industrial equipment, retrieve similar equipment:"
             queries = [instruction + " " + item for item in querys]
@@ -73,22 +74,26 @@ for db in sets:
             # Concatenate DataFrames horizontally (along columns)
             df_merged = pd.concat([df_values, df_indices], axis=1)
             df_merged.to_csv(prefixmatch + ".csv", index=False)
-        elif mdl['type'] == 'ST':
-            sentence_model = SentenceTransformer(mdl['mdl'])
-            prefixmatch = mdl['mode'] + '_' + db['mode']
+        elif mdl["type"] == "ST":
+            sentence_model = SentenceTransformer(mdl["mdl"])
+            prefixmatch = mdl["mode"] + "_" + db["mode"]
 
-            df = pd.read_csv(db['train'])
+            df = pd.read_csv(db["train"])
             documents = list(df["TypeData.GenCompType"])
 
-            df = pd.read_csv(db['dest'])
+            df = pd.read_csv(db["dest"])
             querys = list(df["TypeData.GenCompType"])
 
             train_reps = sentence_model.encode(documents)
             test_reps = sentence_model.encode(querys)
 
             # Normalize representations
-            train_reps_norm = torch.nn.functional.normalize(torch.tensor(train_reps), p=2, dim=1)
-            test_reps_norm = torch.nn.functional.normalize(torch.tensor(test_reps), p=2, dim=1)
+            train_reps_norm = torch.nn.functional.normalize(
+                torch.tensor(train_reps), p=2, dim=1
+            )
+            test_reps_norm = torch.nn.functional.normalize(
+                torch.tensor(test_reps), p=2, dim=1
+            )
 
             # Calculate cosine similarity
             cos_sim = torch.mm(test_reps_norm, train_reps_norm.transpose(0, 1))
@@ -109,4 +114,3 @@ for db in sets:
             # Concatenate DataFrames horizontally (along columns)
             df_merged = pd.concat([df_values, df_indices], axis=1)
             df_merged.to_csv(prefixmatch + ".csv", index=False)
-
